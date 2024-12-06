460aff8924d567fb3cae140e803696e702e2dfa0
meex28 <piotrp7392@gmail.com>
2024-12-06 20:33:30 +0100

    Add basic chess logic code

---
+++ app/backend/chess_logic.py
@@ -0,0 +1,46 @@
+import chess
+import chess.svg
+
+def board_from_fen(fen: str) -> chess.Board:
+    return chess.Board(fen)
+
+def str_to_square(square: str) -> chess.Square:
+    return chess.parse_square(square.lower())
+
+def save_svg(svg: str, filename: str):
+    with open(filename, 'w') as f:
+        f.write(svg)
+
+def board_to_svg(
+        board: chess.Board,
+        highlighted_squares: list[tuple[list[chess.Square], str]] = [],
+        size: int =600,
+        orientation: chess.Color =chess.WHITE
+) -> str:
+    fill_dict = {}
+
+    for square_list, color in highlighted_squares:
+        for square in square_list:
+            fill_dict[square] = color
+
+    return chess.svg.board(
+        board=board,
+        size=size,
+        fill=fill_dict,
+        coordinates=True,
+        orientation=orientation
+    )
+
+# Example usage
+def main():
+    starting_fen = chess.STARTING_FEN
+    board = board_from_fen(starting_fen)
+
+    svg = board_to_svg(board, highlighted_squares=[
+        ([str_to_square('d4'), str_to_square('d5')], 'red'),
+        ([str_to_square('e4'), str_to_square('e5')], 'green')
+    ])
+    save_svg(svg, 'sample_board.svg')
+
+if __name__ == "__main__":
+    main()
