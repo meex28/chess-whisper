8f33c9e65b1a07ee14e76390f5102392cd708c8e
meex28 <piotrp7392@gmail.com>
2025-01-14 11:48:18 +0100

    tmp

--- app/service/scenario_flow/callbacks/assistant_text.py
+++ app/service/scenario_flow/callbacks/assistant_text.py
@@ -7,7 +7,7 @@ def build_assistant_text_callback(text: str):
     # TODO: extract function, add loading state and keep conversation in session_state
     # TODO: wait until running next callback until text is finished
     def run():
-        print(f"Playing assistant text: ${text}")
+        print(f"Playing assistant text: {text}")
         voice_synthesis = get_speech_recording(text = text)
 
         if voice_synthesis.success:
