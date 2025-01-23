90f46d675ed398250d0a150570bc34c4726290e7
meex28 <piotrp7392@gmail.com>
2025-01-23 19:12:35 +0100

    add simpler puzzle level

--- app/levels/level10.py
+++ app/levels/level10.py
@@ -16,6 +16,8 @@ from app.service.scenario_flow.handlers.user_confirmation import build_user_conf
 from app.service.scenario_flow.handlers.move_expected import build_user_move_expected_handler
 from app.levels.scenario_builder import build_scenario_step
 
+# puzzle from lichess: http://lichess.org/training/I8Tq9
+
 _scenario_mate_in_one: Scenario = Scenario(steps=[
     # Introduction
     build_scenario_step(
@@ -23,8 +25,7 @@ _scenario_mate_in_one: Scenario = Scenario(steps=[
         callbacks=[
             build_assistant_text_callback("""
             Witaj! W tej lekcji rozwiążesz swoją pierwszą łamigłówkę szachową. 
-            W takich łamigłówkach Twoim zadaniem jest dać mata przeciwnikowi. W tej konkretnej możesz zrobić to w jednym posunięciu. 
-            Przyjrzyj się uważnie pozycji i znajdź zwycięski ruch.
+            W takich łamigłówkach Twoim zadaniem jest dać mata przeciwnikowi. 
             """),
             build_go_to_next_step_callback()
         ],
@@ -35,10 +36,7 @@ _scenario_mate_in_one: Scenario = Scenario(steps=[
         type=ScenarioStepType.BOARD_TRANSFORMATION,
         callbacks=[
             build_board_transformation_callback(transformations=[
-                build_reset_board_transformation(new_board=chess.Board(fen="r5k1/1b4pr/4b1p1/p2pP1Q1/P2P4/2PB1N1P/5PP1/5RK1 b - - 0 1")),
-                build_highlight_squares_board_transformation(highlighted_squares=[
-                    ([str_to_square('g5')], SquareFillColor.YELLOW)
-                ])
+                build_reset_board_transformation(new_board=chess.Board(fen="4b1rk/3q1p2/1p2p1pp/1P1pb3/rB1P3P/2P1Q3/1R3PP1/1R4K1 w - - 0 1")),
             ]),
             build_go_to_next_step_callback()
         ]
@@ -49,6 +47,7 @@ _scenario_mate_in_one: Scenario = Scenario(steps=[
         type=ScenarioStepType.ASSISTANT_TEXT,
         callbacks=[
             build_assistant_text_callback("""
+            W tej łamigłówce możesz zrobić to jednym ruchem.
             Zwróć uwagę na pozycję króla przeciwnika. Pamiętaj, że mat oznacza brak możliwości ucieczki dla króla. 
             Wykonaj ruch, który zakończy grę.
             """),
@@ -61,11 +60,11 @@ _scenario_mate_in_one: Scenario = Scenario(steps=[
         type=ScenarioStepType.USER_ACTION,
         handlers=[
             build_user_move_expected_handler(
-                expected_move=chess.Move.from_uci("g5h6"),
-                unexpected_move_response="Spróbuj znaleźć ruch, który da mata. Przemyśl pozycję jeszcze raz.",
+                expected_move=chess.Move.from_uci("e3h6"),
+                unexpected_move_response="Spróbuj znaleźć ruch, który zaatakuje króla i da mata. Przemyśl pozycję jeszcze raz.",
                 callbacks=[
                     build_board_transformation_callback(transformations=[
-                        build_move_board_transformation(move=chess.Move.from_uci("g5h6")),
+                        build_move_board_transformation(move=chess.Move.from_uci("e3h6")),
                         build_turn_set_transformation(color=chess.BLACK)
                     ]),
                     build_go_to_next_step_callback()
